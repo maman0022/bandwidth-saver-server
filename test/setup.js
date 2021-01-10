58046805f70c05f94d043a07965bfa1a92af50fd
Muhammad Aman <maman0022@outlook.com>
2021-01-10 17:09:12 -0600

    added envs to test setup

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
+process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
+process.env.RECAPTCHA_SECRET = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "postgres://localhost/bandwidth-test"
+
 const { expect } = require('chai')
 const supertest = require('supertest')
 
